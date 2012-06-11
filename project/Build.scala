f630f2792b95b0d87a39d1468b5cae0a04bc1504
Andrew Brandt <Andrew@ansible-kaze.home>
2012-06-11 18:07:58 -0400

    more

--- project/Build.scala
+++ project/Build.scala
@@ -8,8 +8,8 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
-    )
+ 	 "postgresql" % "postgresql" % "8.4-702.jdbc4"
+	)
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
       // Add your own project settings here      
